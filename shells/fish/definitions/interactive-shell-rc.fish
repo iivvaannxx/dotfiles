fca9dac54a5eeae1ba4362a55f4ce7b8501a7fbc
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-03-03 14:09:13 +0100

    Added some environment variables.

--- shells/fish/definitions/interactive-shell-rc.fish
+++ shells/fish/definitions/interactive-shell-rc.fish
@@ -1,9 +1,9 @@
 # Initialization code sourced at every interactive fish shell (usually on terminals).
 
-# Disable the greeting.
-set fish_greeting ""
-
 # Remap the bindings for the fzf fish plugin.
 if type -q fzf_configure_bindings
     fzf_configure_bindings --history=\e\ch --variables=\e\cv
 end
+
+# Disable the greeting.
+set fish_greeting ""
--- shells/fish/definitions/shell-rc.fish
+++ shells/fish/definitions/shell-rc.fish
@@ -1,3 +1,11 @@
 # Initialization code sourced at every fish shell (interactive or non-interactive).
 
+# Environment variables.
 set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
+
+set -x EDITOR nvim
+set -x VISUAL code
+set -x BROWSER firefox
+
+set -x NIXOS_CONFIG $HOME/.nixos-config
+set -x DOTFILES $HOME/.dotfiles

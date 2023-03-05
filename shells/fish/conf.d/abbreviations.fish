333270eb3749110821c344380b32497adb695da1
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-03-05 19:55:43 +0100

    Testing abbreviation parsing.

--- shells/fish/conf.d/abbreviations.fish
+++ shells/fish/conf.d/abbreviations.fish
@@ -1,13 +1,3 @@
-# Initialization code sourced at every interactive fish shell (usually on terminals).
-
-# Remap the bindings for the fzf fish plugin.
-if type -q fzf_configure_bindings
-    fzf_configure_bindings --history=\e\ch --variables=\e\cv
-end
-
-# Disable the greeting.
-set fish_greeting ""
-
 status --is-interactive; and begin
 
     set abbreviations $HOME/.dotfiles/shells/fish/manifests/abbreviations.json

de759602c354876620068bafb015735d75f03a1f
Ivan Porto Wigner <47715589+iivvaannxx@users.noreply.github.com>
2023-07-16 01:12:14 +0200

    Modified default browser.

--- shells/fish/init/shell-rc.fish
+++ shells/fish/init/shell-rc.fish
@@ -3,7 +3,7 @@ set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
 
 set -x EDITOR nvim
 set -x VISUAL code
-set -x BROWSER firefox
+set -x BROWSER brave
 
 set -x NIXOS_CONFIG $HOME/.nixos-config
 set -x DOTFILES $HOME/.dotfiles
